d51154a2c70fc3cbc0624bfebb6d7b47628f400d
Qiujunhao <13253571465@163.com>
2022-07-10 17:16:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import sun.applet.Main;
+
+/**
+ * @author shkstart
+ * @create 2022-07-10 17:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
