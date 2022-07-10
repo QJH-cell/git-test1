8433a0864ebe77f4391c8612b375058a8c5b945c
Qiujunhao <13253571465@163.com>
2022-07-10 17:55:43 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,7 +13,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git4-master");
-        System.out.println("hello git4-hotfix");
-
+        System.out.println("hello git4-master");
+        System.out.println("push test");
     }
 }
