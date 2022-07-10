75abcef99cfa00e8056863cc6102f35492b8543f
Qiujunhao <13253571465@163.com>
2022-07-10 17:17:48 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ import sun.applet.Main;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
 
     }
 }
