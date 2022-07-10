978a2f1e32ceca5bbb7b3ab2e2849291f6caa18f
Qiujunhao <13253571465@163.com>
2022-07-10 17:18:48 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,8 +8,9 @@ import sun.applet.Main;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
